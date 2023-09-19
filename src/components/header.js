4e0563834d0fc28b47aacf6e3943271aea7c4c4c
Harshit M <harshitmeh111@gmail.com>
2023-09-19 20:57:32 +0530

    Added code

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header className='app-header'>
+        <img src='jlug-logo.jpeg' alt='JLUG logo' />
+        <h1>The JLUG Quiz</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
